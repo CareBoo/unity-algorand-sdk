c03aec83ba0f4469c2ce903c2ef30d20d92fb732
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-07-12 00:41:26 -0700

    test: :white_check_mark: Add mnemonic equality test

--- Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk.Tests/MnemonicTest.cs
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk.Tests/MnemonicTest.cs
@@ -4,12 +4,31 @@ using NUnit.Framework;
 
 public class MnemonicTest
 {
+    private string RandomMnemonicString()
+    {
+        var words = new string[Mnemonic.Length];
+        var random = new Random();
+        for (var i = 0; i < Mnemonic.Length; i++)
+            words[i] = ((Mnemonic.Word)random.Next((int)Mnemonic.Word.LENGTH)).ToString();
+        return string.Join(" ", words);
+    }
+
     [Test]
     public void CanGenerateMnemonicFromString()
     {
-        var expected = "any spoon travel skin conduct release road desk relief attend noodle error clock limit agree brave bright industry eternal stuff despair virus kitten abandon faith";
+        var expected = RandomMnemonicString();
         var mnemonic = Mnemonic.FromString(expected);
         Assert.AreEqual(expected, mnemonic.ToString());
+    }
+
+    [Test]
+    public void MnemonicsFromTheSameStringShouldBeEqual()
+    {
+        var mnemonicString = RandomMnemonicString();
+        var mnemonic1 = Mnemonic.FromString(mnemonicString);
+        var mnemonic2 = Mnemonic.FromString(mnemonicString);
+        Assert.IsTrue(mnemonic1.Equals(mnemonic2));
+        Assert.IsTrue(mnemonic2.Equals(mnemonic1));
     }
 
     [Test]
