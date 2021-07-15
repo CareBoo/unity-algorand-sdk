066a8ad97732d3358ed9de8282c298015c95ccf6
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-07-14 23:04:55 -0700

    feat: :heavy_plus_sign: Add libsodium ios, android, and windows

--- Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk.Crypto.Tests/Sha512Test.cs
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk.Crypto.Tests/Sha512Test.cs
@@ -1,17 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Algorand.Crypto;
+using AlgoSdk.Crypto;
 using System.Text;
 using NUnit.Framework;
 
-public class Sha512Test : MonoBehaviour
+public class Sha512Test
 {
     [Test]
     public void Sha512IsTruncatedTo32Bytes()
     {
         var bytes = new byte[] { 0x30, 0x30, 0x30, 0x30 };
-        bytes = Algorand.Crypto.SHA512.Hash(bytes);
+        bytes = AlgoSdk.Crypto.SHA512.Hash(bytes);
         Assert.AreEqual(32, bytes.Length);
     }
 }
--- Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk.Crypto/SHA512.cs
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk.Crypto/SHA512.cs
@@ -1,15 +1,12 @@
-using NSec.Cryptography;
 using System;
 
-namespace Algorand.Crypto
+namespace AlgoSdk.Crypto
 {
     public static class SHA512
     {
         public static byte[] Hash(byte[] data)
         {
-            var algorithm = HashAlgorithm.Sha512_256;
-            var readonlyspan = new ReadOnlySpan<byte>(data);
-            return algorithm.Hash(readonlyspan);
+            throw new NotImplementedException();
         }
     }
 }
