063aae685dfafbe962b792555e2d9ddf726833d9
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-08-18 19:58:15 -0700

    feat(algod): :sparkles: Implement AssetParams

--- Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/Algod/Models/AssetParams.cs
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/Algod/Models/AssetParams.cs
@@ -1,13 +1,29 @@
 using System;
 using AlgoSdk.MsgPack;
+using Unity.Collections;
 
 namespace AlgoSdk
 {
-    public partial struct AssetParams : IEquatable<AssetParams>, IMessagePackObject
+    public partial struct AssetParams
+        : IEquatable<AssetParams>
+        , IMessagePackObject
     {
+        public Address Clawback;
+        public Address Creator;
+        public ulong Decimals;
+        public Optional<bool> DefaultFrozen;
+        public Address Freeze;
+        public Address Manager;
+        public FixedString128 MetadataHash;
+        public FixedString64 Name;
+        public Address Reserve;
+        public ulong Total;
+        public FixedString64 UnitName;
+        public FixedString512 Url;
+
         public bool Equals(AssetParams other)
         {
-            return true;
+            return this.Equals(ref other);
         }
     }
 }
@@ -16,5 +32,20 @@ namespace AlgoSdk.MsgPack
 {
     internal static partial class FieldMaps
     {
+        internal static readonly Field<AssetParams>.Map assetParamsFields =
+            new Field<AssetParams>.Map()
+                .Assign("clawback", (ref AssetParams x) => ref x.Clawback)
+                .Assign("creator", (ref AssetParams x) => ref x.Creator)
+                .Assign("decimals", (ref AssetParams x) => ref x.Decimals)
+                .Assign("default-frozen", (ref AssetParams x) => ref x.DefaultFrozen)
+                .Assign("freeze", (ref AssetParams x) => ref x.Freeze)
+                .Assign("manager", (ref AssetParams x) => ref x.Manager)
+                .Assign("metadata-hash", (ref AssetParams x) => ref x.MetadataHash)
+                .Assign("name", (ref AssetParams x) => ref x.Name)
+                .Assign("reserve", (ref AssetParams x) => ref x.Reserve)
+                .Assign("total", (ref AssetParams x) => ref x.Total)
+                .Assign("unit-name", (ref AssetParams x) => ref x.UnitName)
+                .Assign("url", (ref AssetParams x) => ref x.Url)
+                ;
     }
 }
