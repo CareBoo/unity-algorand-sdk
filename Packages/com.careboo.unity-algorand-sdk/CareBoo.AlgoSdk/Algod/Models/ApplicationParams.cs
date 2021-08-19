0888bfde98bdc2e7e8814da8b942a348cbc3a358
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-08-18 19:41:54 -0700

    feat(algod): :sparkles: Implement ApplicationParams

--- Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/Algod/Models/ApplicationParams.cs
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/Algod/Models/ApplicationParams.cs
@@ -1,7 +1,44 @@
+using System;
+using AlgoSdk.MsgPack;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Jobs;
+
 namespace AlgoSdk
 {
     public struct ApplicationParams
+        : IMessagePackObject
+        , IEquatable<ApplicationParams>
+        , INativeDisposable
     {
+        public UnsafeList<byte> ApprovalProgram;
+        public UnsafeList<byte> ClearStateProgram;
+        public Address Creator;
+        public Optional<ulong> ExtraProgramPages;
+        public UnsafeList<TealKeyValue> GlobalState;
+        public Optional<ApplicationStateSchema> GlobalStateSchema;
+        public Optional<ApplicationStateSchema> LocalStateSchema;
+
+        public JobHandle Dispose(JobHandle inputDeps)
+        {
+            return JobHandle.CombineDependencies(
+                ApprovalProgram.Dispose(inputDeps),
+                ClearStateProgram.Dispose(inputDeps),
+                GlobalState.Dispose(inputDeps)
+            );
+        }
+
+        public void Dispose()
+        {
+            ApprovalProgram.Dispose();
+            ClearStateProgram.Dispose();
+            GlobalState.Dispose();
+        }
+
+        public bool Equals(ApplicationParams other)
+        {
+            return this.Equals(ref other);
+        }
     }
 }
 
@@ -9,5 +46,15 @@ namespace AlgoSdk.MsgPack
 {
     internal static partial class FieldMaps
     {
+        internal static readonly Field<ApplicationParams>.Map applicationParamsFields =
+            new Field<ApplicationParams>.Map()
+                .Assign("approval-program", (ref ApplicationParams x) => ref x.ApprovalProgram, default(UnsafeListComparer<byte>))
+                .Assign("clear-state-program", (ref ApplicationParams x) => ref x.ClearStateProgram, default(UnsafeListComparer<byte>))
+                .Assign("creator", (ref ApplicationParams x) => ref x.Creator)
+                .Assign("extra-program-pages", (ref ApplicationParams x) => ref x.ExtraProgramPages)
+                .Assign("global-state", (ref ApplicationParams x) => ref x.GlobalState, default(UnsafeListComparer<TealKeyValue>))
+                .Assign("global-state-schema", (ref ApplicationParams x) => ref x.GlobalStateSchema)
+                .Assign("local-state-schema", (ref ApplicationParams x) => ref x.LocalStateSchema)
+                ;
     }
 }
