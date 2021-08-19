ce13e4e2c2848212cb2a2dc01fee279b861c9daa
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-08-18 19:41:39 -0700

    feat(algod): :sparkles: Implement ApplicationLocalState

--- Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/Algod/Models/ApplicationLocalState.cs
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/Algod/Models/ApplicationLocalState.cs
@@ -1,7 +1,34 @@
+using System;
+using AlgoSdk.MsgPack;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Jobs;
+
 namespace AlgoSdk
 {
     public struct ApplicationLocalState
+        : IMessagePackObject
+        , IEquatable<ApplicationLocalState>
+        , INativeDisposable
     {
+        public ulong Id;
+        public UnsafeList<TealKeyValue> KeyValues;
+        public ApplicationStateSchema Schema;
+
+        public JobHandle Dispose(JobHandle inputDeps)
+        {
+            return KeyValues.Dispose(inputDeps);
+        }
+
+        public void Dispose()
+        {
+            KeyValues.Dispose();
+        }
+
+        public bool Equals(ApplicationLocalState other)
+        {
+            return this.Equals(ref other);
+        }
     }
 }
 
@@ -9,5 +36,11 @@ namespace AlgoSdk.MsgPack
 {
     internal static partial class FieldMaps
     {
+        internal static readonly Field<ApplicationLocalState>.Map applicationLocalStateFields =
+            new Field<ApplicationLocalState>.Map()
+                .Assign("id", (ref ApplicationLocalState x) => ref x.Id)
+                .Assign("key-value", (ref ApplicationLocalState x) => ref x.KeyValues, default(UnsafeListComparer<TealKeyValue>))
+                .Assign("schema", (ref ApplicationLocalState x) => ref x.Schema)
+                ;
     }
 }
