752fee6cbab899a48c2bf6f6673114f831a273e3
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-08-18 19:41:24 -0700

    feat: :sparkles: Implement Application

--- Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/Algod/Models/Application.cs
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/Algod/Models/Application.cs
@@ -1,7 +1,19 @@
+using System;
+using AlgoSdk.MsgPack;
+
 namespace AlgoSdk
 {
     public struct Application
+        : IMessagePackObject
+        , IEquatable<Application>
     {
+        public ulong Id;
+        public ApplicationParams Params;
+
+        public bool Equals(Application other)
+        {
+            return this.Equals(ref other);
+        }
     }
 }
 
@@ -9,5 +21,10 @@ namespace AlgoSdk.MsgPack
 {
     internal static partial class FieldMaps
     {
+        internal readonly static Field<Application>.Map applicationFields =
+            new Field<Application>.Map()
+                .Assign("id", (ref Application x) => ref x.Id)
+                .Assign("params", (ref Application x) => ref x.Params)
+                ;
     }
 }
