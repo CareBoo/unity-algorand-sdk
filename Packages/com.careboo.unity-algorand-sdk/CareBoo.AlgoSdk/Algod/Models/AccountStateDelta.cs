1b327aa71a4fede26123ae8d0864c6d57d3e8030
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-08-18 19:41:11 -0700

    feat(algod): :sparkles: Implement AccountStateDelta

--- Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/Algod/Models/AccountStateDelta.cs
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/Algod/Models/AccountStateDelta.cs
@@ -1,7 +1,19 @@
+using System;
+using AlgoSdk.MsgPack;
+
 namespace AlgoSdk
 {
     public struct AccountStateDelta
+        : IMessagePackObject
+        , IEquatable<AccountStateDelta>
     {
+        public Address Address;
+        public StateDelta Delta;
+
+        public bool Equals(AccountStateDelta other)
+        {
+            return this.Equals(ref other);
+        }
     }
 }
 
@@ -9,5 +21,10 @@ namespace AlgoSdk.MsgPack
 {
     internal static partial class FieldMaps
     {
+        internal static readonly Field<AccountStateDelta>.Map accountStateDeltaFields =
+            new Field<AccountStateDelta>.Map()
+                .Assign("address", (ref AccountStateDelta x) => ref x.Address)
+                .Assign("delta", (ref AccountStateDelta x) => ref x.Delta)
+                ;
     }
 }
