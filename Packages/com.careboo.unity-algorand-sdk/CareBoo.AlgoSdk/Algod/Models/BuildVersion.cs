6f7b64ba0c71dea94a736ac738d64e471edeefea
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-08-18 21:15:27 -0700

    feat(algod): :sparkles: Implement BuildVersion

--- Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/Algod/Models/BuildVersion.cs
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/Algod/Models/BuildVersion.cs
@@ -1,7 +1,24 @@
+using System;
+using AlgoSdk.MsgPack;
+using Unity.Collections;
+
 namespace AlgoSdk
 {
     public struct BuildVersion
+        : IMessagePackObject
+        , IEquatable<BuildVersion>
     {
+        public FixedString64 Branch;
+        public ulong BuildNumber;
+        public FixedString64 Channel;
+        public FixedString128 CommitHash;
+        public ulong Major;
+        public ulong Minor;
+
+        public bool Equals(BuildVersion other)
+        {
+            return this.Equals(ref other);
+        }
     }
 }
 
@@ -9,5 +26,14 @@ namespace AlgoSdk.MsgPack
 {
     internal static partial class FieldMaps
     {
+        internal readonly static Field<BuildVersion>.Map buildVersionFields =
+            new Field<BuildVersion>.Map()
+                .Assign("branch", (ref BuildVersion x) => ref x.Branch)
+                .Assign("build_number", (ref BuildVersion x) => ref x.BuildNumber)
+                .Assign("channel", (ref BuildVersion x) => ref x.Channel)
+                .Assign("commit_hash", (ref BuildVersion x) => ref x.CommitHash)
+                .Assign("major", (ref BuildVersion x) => ref x.Major)
+                .Assign("minor", (ref BuildVersion x) => ref x.Minor)
+                ;
     }
 }
