9c16f22483f32d48c04ee7d18edc46dd6c54053b
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-08-18 21:18:34 -0700

    feat(algod): :sparkles: Implement CatchupMessage

--- Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/Algod/Models/CatchupMessage.cs
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/Algod/Models/CatchupMessage.cs
@@ -1,7 +1,19 @@
+using System;
+using AlgoSdk.MsgPack;
+using Unity.Collections;
+
 namespace AlgoSdk
 {
     public struct CatchupMessage
+        : IMessagePackObject
+        , IEquatable<CatchupMessage>
     {
+        public FixedString512 Message;
+
+        public bool Equals(CatchupMessage other)
+        {
+            return this.Equals(ref other);
+        }
     }
 }
 
@@ -9,5 +21,9 @@ namespace AlgoSdk.MsgPack
 {
     internal static partial class FieldMaps
     {
+        internal static readonly Field<CatchupMessage>.Map catchupMessageFields =
+            new Field<CatchupMessage>.Map()
+                .Assign("catchup-message", (ref CatchupMessage x) => ref x.Message)
+                ;
     }
 }
