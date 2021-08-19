7f968d1923e7ef218139b95c5f827582ac6ff748
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-08-18 19:42:22 -0700

    feat(algod): :sparkles: Implement Asset

--- Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/Algod/Models/Asset.cs
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/Algod/Models/Asset.cs
@@ -1,7 +1,19 @@
+using System;
+using AlgoSdk.MsgPack;
+
 namespace AlgoSdk
 {
     public struct Asset
+        : IMessagePackObject
+        , IEquatable<Asset>
     {
+        public ulong Index;
+        public AssetParams Params;
+
+        public bool Equals(Asset other)
+        {
+            return this.Equals(ref other);
+        }
     }
 }
 
@@ -9,5 +21,10 @@ namespace AlgoSdk.MsgPack
 {
     internal static partial class FieldMaps
     {
+        internal static readonly Field<Asset>.Map assetFields =
+            new Field<Asset>.Map()
+                .Assign("index", (ref Asset x) => ref x.Index)
+                .Assign("params", (ref Asset x) => ref x.Params)
+                ;
     }
 }
