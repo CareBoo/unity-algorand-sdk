3e1f309cb8f64295e5af58669419280f38f5546e
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-08-21 00:02:27 -0700

    ci(test): add conditonal ignore for sandbox tests

--- Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk.Tests/AlgodClientTest.cs
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk.Tests/AlgodClientTest.cs
@@ -12,6 +12,7 @@ public class AlgodClientTest
     static readonly AlgodClient client = new AlgodClient(SandBoxAddress, SandboxToken);
 
     [UnityTest]
+    [ConditionalIgnore(nameof(UnityEngine.Application.isBatchMode), "This test requires certain dependencies to be set up and running.")]
     public IEnumerator SandboxShouldBeHealthy() => UniTask.ToCoroutine(async () =>
     {
         var expected = "null\n";
@@ -20,6 +21,7 @@ public class AlgodClientTest
     });
 
     [UnityTest]
+    [ConditionalIgnore(nameof(UnityEngine.Application.isBatchMode), "This test requires certain dependencies to be set up and running.")]
     public IEnumerator PlayException() => UniTask.ToCoroutine(async () =>
     {
         using var response = await client.GetAsync("dne");
