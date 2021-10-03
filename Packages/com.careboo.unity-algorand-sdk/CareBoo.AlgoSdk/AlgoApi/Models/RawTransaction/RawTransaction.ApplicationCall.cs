44029ca8048cd9baf31d1a9aff9eea3b335ec93d
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-10-02 23:11:09 -0700

    feat(indexer): :sparkles: implement `Transaction.KeyRegistration` model fields

--- Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/AlgoApi/Models/RawTransaction/RawTransaction.ApplicationCall.cs
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/AlgoApi/Models/RawTransaction/RawTransaction.ApplicationCall.cs
@@ -2,77 +2,77 @@ namespace AlgoSdk
 {
     public partial struct RawTransaction
     {
-        [AlgoApiField("application-id", "apid")]
+        [AlgoApiField(null, "apid")]
         public ulong ApplicationId
         {
             get => ApplicationCallParams.ApplicationId;
             set => ApplicationCallParams.ApplicationId = value;
         }
 
-        [AlgoApiField("on-completion", "apan")]
+        [AlgoApiField(null, "apan")]
         public ulong OnComplete
         {
             get => ApplicationCallParams.OnComplete;
             set => ApplicationCallParams.OnComplete = value;
         }
 
-        [AlgoApiField("accounts", "apat")]
+        [AlgoApiField(null, "apat")]
         public Address[] Accounts
         {
             get => ApplicationCallParams.Accounts;
             set => ApplicationCallParams.Accounts = value;
         }
 
-        [AlgoApiField("approval-program", "apap")]
+        [AlgoApiField(null, "apap")]
         public byte[] ApprovalProgram
         {
             get => ApplicationCallParams.ApprovalProgram;
             set => ApplicationCallParams.ApprovalProgram = value;
         }
 
-        [AlgoApiField("application-args", "apaa")]
+        [AlgoApiField(null, "apaa")]
         public byte[] AppArguments
         {
             get => ApplicationCallParams.AppArguments;
             set => ApplicationCallParams.AppArguments = value;
         }
 
-        [AlgoApiField("clear-state-program", "apsu")]
+        [AlgoApiField(null, "apsu")]
         public byte[] ClearStateProgram
         {
             get => ApplicationCallParams.ClearStateProgram;
             set => ApplicationCallParams.ClearStateProgram = value;
         }
 
-        [AlgoApiField("foreign-apps", "apfa")]
+        [AlgoApiField(null, "apfa")]
         public Address[] ForeignApps
         {
             get => ApplicationCallParams.ForeignApps;
             set => ApplicationCallParams.ForeignApps = value;
         }
 
-        [AlgoApiField("foreign-assets", "apas")]
+        [AlgoApiField(null, "apas")]
         public Address[] ForeignAssets
         {
             get => ApplicationCallParams.ForeignAssets;
             set => ApplicationCallParams.ForeignAssets = value;
         }
 
-        [AlgoApiField("global-state-schema", "apgs")]
+        [AlgoApiField(null, "apgs")]
         public Optional<ApplicationStateSchema> GlobalStateSchema
         {
             get => ApplicationCallParams.GlobalStateSchema;
             set => ApplicationCallParams.GlobalStateSchema = value;
         }
 
-        [AlgoApiField("local-state-schema", "apls")]
+        [AlgoApiField(null, "apls")]
         public Optional<ApplicationStateSchema> LocalStateSchema
         {
             get => ApplicationCallParams.LocalStateSchema;
             set => ApplicationCallParams.LocalStateSchema = value;
         }
 
-        [AlgoApiField("extra-program-pages", "apep")]
+        [AlgoApiField(null, "apep")]
         public Optional<ulong> ExtraProgramPages
         {
             get => ApplicationCallParams.ExtraProgramPages;
