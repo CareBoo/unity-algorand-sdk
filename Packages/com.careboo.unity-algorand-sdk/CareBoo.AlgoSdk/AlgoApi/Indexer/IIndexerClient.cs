3bc4c45221247b7199e8a3777cf7370fee6ca996
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-10-03 09:49:27 -0700

    feat(indexer): :sparkles: implement `AccountQuery`

--- Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/AlgoApi/Indexer/IIndexerClient.cs
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/AlgoApi/Indexer/IIndexerClient.cs
@@ -5,5 +5,6 @@ namespace AlgoSdk
     public interface IIndexerClient
     {
         UniTask<AlgoApiResponse<HealthCheck>> GetHealth();
+        UniTask<AlgoApiResponse<AccountsResponse>> GetAccounts(AccountsQuery query = default);
     }
 }
