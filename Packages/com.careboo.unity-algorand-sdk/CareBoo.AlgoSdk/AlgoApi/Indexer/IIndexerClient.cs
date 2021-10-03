c383659059b43efe5d2f95c113f0285147e1b199
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-10-03 11:23:38 -0700

    feat(indexer): :sparkles: add `IIndexerClient`

--- Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/AlgoApi/Indexer/IIndexerClient.cs
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/AlgoApi/Indexer/IIndexerClient.cs
@@ -6,6 +6,16 @@ namespace AlgoSdk
     {
         UniTask<AlgoApiResponse<HealthCheck>> GetHealth();
         UniTask<AlgoApiResponse<AccountsResponse>> GetAccounts(AccountsQuery query = default);
-        UniTask<AlgoApiResponse<Account>> GetAccount(ulong accountId, AccountQuery query = default);
+        UniTask<AlgoApiResponse<AccountResponse>> GetAccount(ulong accountId, AccountQuery query = default);
+        UniTask<AlgoApiResponse<TransactionsResponse>> GetAccountTransactions(ulong accountId, TransactionsQuery query = default);
+        UniTask<AlgoApiResponse<ApplicationsResponse>> GetApplications(ApplicationsQuery query = default);
+        UniTask<AlgoApiResponse<ApplicationResponse>> GetApplication(ulong applicationId, ApplicationsQuery query = default);
+        UniTask<AlgoApiResponse<AssetsResponse>> GetAssets(AssetsQuery query = default);
+        UniTask<AlgoApiResponse<AssetResponse>> GetAsset(ulong assetId, AssetQuery query = default);
+        UniTask<AlgoApiResponse<BalancesResponse>> GetAssetBalances(ulong assetId, BalancesQuery query = default);
+        UniTask<AlgoApiResponse<TransactionsResponse>> GetAssetTransactions(ulong assetId, TransactionsQuery query = default);
+        UniTask<AlgoApiResponse<Block>> GetBlock(ulong round);
+        UniTask<AlgoApiResponse<TransactionsResponse>> GetTransactions(TransactionsQuery query = default);
+        UniTask<AlgoApiResponse<TransactionResponse>> GetTransaction(TransactionId txid);
     }
 }
