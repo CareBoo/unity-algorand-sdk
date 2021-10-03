2432bdd1ec1a4a105bea297b413a80921a9d70ee
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-10-03 02:48:57 -0700

    refactor: :fire: remove some verify methods in the transaction
    the removed methods aren't really that useful, since the node should be handling the verification
    
    BREAKING CHANGE: remove `Transaction.VerifySignature`, `SignedTransaction.Verify`

--- Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk.Tests/SignedTransactionTest.cs
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk.Tests/SignedTransactionTest.cs
@@ -7,23 +7,6 @@ using Unity.Collections.LowLevel.Unsafe;
 public class SignedTransactionTest
 {
     [Test]
-    public void SignedTransactionShouldBeVerifiable()
-    {
-        var seed = AlgoSdk.Crypto.Random.Bytes<PrivateKey>();
-        using var kp = seed.ToKeyPair();
-        var transaction = new Transaction.Payment(
-            fee: 40000,
-            firstValidRound: 123,
-            genesisHash: AlgoSdk.Crypto.Random.Bytes<Sha512_256_Hash>(),
-            lastValidRound: 124,
-            sender: (Address)kp.PublicKey,
-            receiver: AlgoSdk.Crypto.Random.Bytes<Address>().GenerateCheckSum(),
-            amount: 1000000);
-        var signedTransaction = transaction.Sign(kp.SecretKey);
-        Assert.IsTrue(signedTransaction.Verify());
-    }
-
-    [Test]
     public void SizeOfSignedTransactionShouldBeUnder4KB()
     {
         const int MAX_BYTES = 4000;
