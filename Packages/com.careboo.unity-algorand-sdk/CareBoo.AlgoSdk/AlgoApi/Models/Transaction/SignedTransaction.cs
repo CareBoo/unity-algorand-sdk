57bb3cca8394f96efe7885d0f2df9d0ed0a25d81
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-10-03 02:46:47 -0700

    refactor: :recycle: convert all `SignedTransaction<>` to a single `SignedTransaction`
    Also remove `RawSignedTransaction` -> `SignedTransaction`
    
    BREAKING CHANGE: signed transactions are now represented by `SignedTransaction` (non-generic)

--- Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/AlgoApi/Models/Transaction/SignedTransaction.cs
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/AlgoApi/Models/Transaction/SignedTransaction.cs
@@ -1,19 +1,7 @@
 using System;
-using AlgoSdk.Formatters;
-using Unity.Collections;
 
 namespace AlgoSdk
 {
-    public interface ISignedTransaction
-    {
-        ISignature Signature { get; }
-        ITransaction Transaction { get; }
-        bool Verify();
-
-        public void CopyTo(ref SignedTransaction data);
-        public void CopyFrom(in SignedTransaction data);
-    }
-
     [AlgoApiObject]
     public struct SignedTransaction
         : IEquatable<SignedTransaction>
@@ -48,65 +36,12 @@ namespace AlgoSdk
                 && Transaction.Signature.Equals(other.Transaction.Signature)
                 ;
         }
-    }
 
-    [AlgoApiFormatter(typeof(SignedTransactionFormatter<Transaction.Payment>))]
-    [AlgoApiFormatter(typeof(SignedTransactionFormatter<Transaction.AssetConfiguration>))]
-    [AlgoApiFormatter(typeof(SignedTransactionFormatter<Transaction.AssetFreeze>))]
-    [AlgoApiFormatter(typeof(SignedTransactionFormatter<Transaction.AssetTransfer>))]
-    [AlgoApiFormatter(typeof(SignedTransactionFormatter<Transaction.ApplicationCall>))]
-    [AlgoApiFormatter(typeof(SignedTransactionFormatter<Transaction.KeyRegistration>))]
-    public struct SignedTransaction<TTransaction>
-        : ISignedTransaction
-        , IEquatable<SignedTransaction<TTransaction>>
-        where TTransaction : struct, ITransaction, IEquatable<TTransaction>
-    {
-        public Sig Signature;
-        public TTransaction Transaction;
+        public bool Verify() => Transaction.VerifySignature();
 
-        public SignedTransaction(
-            in Sig signature,
-            in TTransaction transaction
-        )
+        public static implicit operator SignedTransaction(Transaction txn)
         {
-            Signature = signature;
-            Transaction = transaction;
-        }
-
-        ISignature ISignedTransaction.Signature => Signature;
-
-        ITransaction ISignedTransaction.Transaction => Transaction;
-
-        public void CopyFrom(in SignedTransaction data)
-        {
-            Signature = data.Sig;
-            Transaction.CopyFrom(data.Transaction);
-        }
-
-        public void CopyTo(ref SignedTransaction data)
-        {
-            data.Sig = Signature;
-            Transaction.CopyTo(ref data.Transaction);
-        }
-
-        public SignedTransaction ToRaw()
-        {
-            SignedTransaction result = new SignedTransaction();
-            CopyTo(ref result);
-            return result;
-        }
-
-        public bool Equals(SignedTransaction<TTransaction> other)
-        {
-            return Signature.Equals(other.Signature)
-                && Transaction.Equals(other.Transaction)
-                ;
-        }
-
-        public bool Verify()
-        {
-            using var message = Transaction.ToSignatureMessage(Allocator.Temp);
-            return Signature.Verify(message, Transaction.Header.Sender);
+            return new SignedTransaction { Transaction = txn };
         }
     }
 }
