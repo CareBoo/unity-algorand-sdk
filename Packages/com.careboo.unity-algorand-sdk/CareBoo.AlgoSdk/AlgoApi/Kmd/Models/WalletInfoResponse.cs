5bbf76a8b7b61029e51f85e0f286b05677fe13a4
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-10-03 17:58:03 -0700

    feat(kmd): :sparkles: implement `WalletInfoResponse`

---
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/AlgoApi/Kmd/Models/WalletInfoResponse.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace AlgoSdk
+{
+    [AlgoApiObject]
+    public struct WalletInfoResponse
+        : IEquatable<WalletInfoResponse>
+    {
+        [AlgoApiField("wallet_handle", null)]
+        public WalletHandle WalletHandle;
+
+        [AlgoApiField("error", null)]
+        public Optional<bool> Error;
+
+        [AlgoApiField("message", null)]
+        public string Message;
+
+        public bool Equals(WalletInfoResponse other)
+        {
+            return WalletHandle.Equals(other.WalletHandle)
+                && Error.Equals(other.Error)
+                && Message.Equals(other.Message)
+                ;
+        }
+    }
+}
