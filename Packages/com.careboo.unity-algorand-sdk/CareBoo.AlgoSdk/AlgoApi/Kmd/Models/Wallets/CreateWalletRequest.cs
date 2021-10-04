9f1b6672861aae64f636bd6ec346d4efdea38f1a
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-10-03 18:17:07 -0700

    feat(kmd): :sparkles: implement `CreateWalletRequest` api fields

--- Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/AlgoApi/Kmd/Models/Wallets/CreateWalletRequest.cs
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/AlgoApi/Kmd/Models/Wallets/CreateWalletRequest.cs
@@ -3,15 +3,20 @@ using Unity.Collections;
 
 namespace AlgoSdk
 {
+    [AlgoApiObject]
     public struct CreateWalletRequest
         : IEquatable<CreateWalletRequest>
     {
+        [AlgoApiField("master_derivation_key", null)]
         public PrivateKey MasterDerivationKey;
 
+        [AlgoApiField("wallet_driver_name", null)]
         public FixedString64Bytes WalletDriverName;
 
+        [AlgoApiField("wallet_name", null)]
         public FixedString64Bytes WalletName;
 
+        [AlgoApiField("wallet_password", null)]
         public FixedString128Bytes WalletPassword;
 
         public bool Equals(CreateWalletRequest other)
