00b78dc5f56b425797ff43ec893d835c3bdf4af5
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-10-04 23:12:37 -0700

    feat(kmd): :sparkles: implement `KmdClient`
    Fixes #13

--- Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/AlgoApi/Kmd/Models/Keys/GenerateKeyRequest.cs
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/AlgoApi/Kmd/Models/Keys/GenerateKeyRequest.cs
@@ -4,8 +4,8 @@ using Unity.Collections;
 namespace AlgoSdk
 {
     [AlgoApiObject]
-    public struct GenerateMasterKeyRequest
-        : IEquatable<GenerateMasterKeyRequest>
+    public struct GenerateKeyRequest
+        : IEquatable<GenerateKeyRequest>
     {
 
         [AlgoApiField("display_mnemonic", null)]
@@ -14,7 +14,7 @@ namespace AlgoSdk
         [AlgoApiField("wallet_password", null)]
         public FixedString128Bytes WalletPassword;
 
-        public bool Equals(GenerateMasterKeyRequest other)
+        public bool Equals(GenerateKeyRequest other)
         {
             return DisplayMnemonic.Equals(other.DisplayMnemonic)
                 && WalletPassword.Equals(other.WalletPassword)
--- Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/AlgoApi/Shared/IAlgoApiClient.cs
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/AlgoApi/Shared/IAlgoApiClient.cs
@@ -4,63 +4,79 @@ using Unity.Collections;
 
 namespace AlgoSdk
 {
-    public interface IApiClient
+    public interface IAlgoApiClient
     {
         string Address { get; }
         string Token { get; }
     }
 
-    public static class ApiClientExtensions
+    public static class AlgoApiClientExtensions
     {
         public static async UniTask<AlgoApiResponse> GetAsync<T>(this T client, string endpoint)
-            where T : IApiClient
+            where T : IAlgoApiClient
         {
             return await AlgoApiRequest.Get(client.Token, client.GetUrl(endpoint)).Send();
         }
 
-        public static async UniTask<AlgoApiResponse> GetAsync<T, TQuery>(this T client, string endpoint, TQuery query)
-            where T : IApiClient
-            where TQuery : struct, IEquatable<TQuery>
+        public static async UniTask<AlgoApiResponse> GetAsync<T, TBody>(this T client, string endpoint, TBody body)
+            where T : IAlgoApiClient
+            where TBody : struct, IEquatable<TBody>
         {
-            if (query.Equals(default))
-                return await client.GetAsync(endpoint);
             using var json = new NativeText(Allocator.Persistent);
-            AlgoApiSerializer.SerializeJson(query, json);
+            AlgoApiSerializer.SerializeJson(body, json);
             return await AlgoApiRequest.Get(client.Token, client.GetUrl(endpoint), json).Send();
         }
 
+        public static async UniTask<AlgoApiResponse> PostAsync<T, TBody>(this T client, string endpoint, TBody body)
+            where T : IAlgoApiClient
+            where TBody : struct, IEquatable<TBody>
+        {
+            using var json = new NativeText(Allocator.Persistent);
+            AlgoApiSerializer.SerializeJson(body, json);
+            return await AlgoApiRequest.Post(client.Token, client.GetUrl(endpoint), json).Send();
+        }
+
         public static async UniTask<AlgoApiResponse> PostAsync<T>(this T client, string endpoint)
-            where T : IApiClient
+                where T : IAlgoApiClient
         {
             return await AlgoApiRequest.Post(client.Token, client.GetUrl(endpoint)).Send();
         }
 
         public static async UniTask<AlgoApiResponse> PostAsync<T>(this T client, string endpoint, NativeArray<byte>.ReadOnly data)
-            where T : IApiClient
+            where T : IAlgoApiClient
         {
             return await client.PostAsync(endpoint, data.ToArray());
         }
 
         public static async UniTask<AlgoApiResponse> PostAsync<T>(this T client, string endpoint, byte[] data)
-            where T : IApiClient
+            where T : IAlgoApiClient
         {
             return await AlgoApiRequest.Post(client.Token, client.GetUrl(endpoint), data).Send();
         }
 
         public static async UniTask<AlgoApiResponse> PostAsync<T>(this T client, string endpoint, string source)
-            where T : IApiClient
+            where T : IAlgoApiClient
         {
             return await AlgoApiRequest.Post(client.Token, client.GetUrl(endpoint), source).Send();
         }
 
         public static async UniTask<AlgoApiResponse> DeleteAsync<T>(this T client, string endpoint)
-            where T : IApiClient
+            where T : IAlgoApiClient
         {
             return await AlgoApiRequest.Delete(client.Token, client.GetUrl(endpoint)).Send();
         }
 
+        public static async UniTask<AlgoApiResponse> DeleteAsync<T, TBody>(this T client, string endpoint, TBody body)
+            where T : IAlgoApiClient
+            where TBody : struct, IEquatable<TBody>
+        {
+            using var json = new NativeText(Allocator.Persistent);
+            AlgoApiSerializer.SerializeJson(body, json);
+            return await AlgoApiRequest.Delete(client.Token, client.GetUrl(endpoint), json).Send();
+        }
+
         public static string GetUrl<T>(this T client, string endpoint)
-            where T : IApiClient
+            where T : IAlgoApiClient
         {
             return client.Address + endpoint;
         }
