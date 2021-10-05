5eb5d42d7d67919d9b7357708e5d3a68861877d5
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-10-04 21:58:08 -0700

    feat(kmd): :sparkles: implement `ImportMultiSigRequest`

---
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/AlgoApi/Kmd/Models/Signatures/ImportMultiSigRequest.cs
@@ -0,0 +1,32 @@
+using System;
+using AlgoSdk.Crypto;
+using Unity.Collections;
+
+namespace AlgoSdk
+{
+    [AlgoApiObject]
+    public struct ImportMultiSigRequest
+        : IEquatable<ImportMultiSigRequest>
+    {
+        [AlgoApiField("multisig_version", null)]
+        public byte Version;
+
+        [AlgoApiField("pks", null)]
+        public Ed25519.PublicKey[] PublicKeys;
+
+        [AlgoApiField("threshold", null)]
+        public byte Threshold;
+
+        [AlgoApiField("wallet_handle_token", null)]
+        public FixedString128Bytes WalletHandleToken;
+
+        public bool Equals(ImportMultiSigRequest other)
+        {
+            return Version.Equals(other.Version)
+                && ArrayComparer.Equals(PublicKeys, other.PublicKeys)
+                && Threshold.Equals(other.Threshold)
+                && WalletHandleToken.Equals(other.WalletHandleToken)
+                ;
+        }
+    }
+}
