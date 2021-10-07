424c8b49c2a567c015a356db3b12a4d4d035dd61
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-10-06 23:16:49 -0700

    feat(serialization): :sparkles: add support for generically serializing byte enums to strings
    fixes some bugs with the enums not serializing properly too

---
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/Serialization/Formatters/Enum/OnCompletionFormatter.cs
@@ -0,0 +1,20 @@
+using Unity.Collections;
+
+namespace AlgoSdk.Formatters
+{
+    public sealed class OnCompletionFormatter : ByteEnumFormatter<OnCompletion>
+    {
+        private static readonly FixedString32Bytes[] typeToString = new FixedString32Bytes[]
+        {
+            default,
+            "noop",
+            "optin",
+            "closeout",
+            "clear",
+            "update",
+            "delete"
+        };
+
+        public OnCompletionFormatter() : base(typeToString) { }
+    }
+}
