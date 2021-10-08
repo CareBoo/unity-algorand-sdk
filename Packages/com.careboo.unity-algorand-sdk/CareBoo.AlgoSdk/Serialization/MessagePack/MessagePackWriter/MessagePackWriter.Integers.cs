d66935919094ef99208c425f27edf3c0223e0e2d
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2021-10-07 20:26:00 -0700

    feat(serialization): :sparkles: finish implementing `MessagePackReader.Integers` and `MessagePackWriter.Integers`

--- Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/Serialization/MessagePack/MessagePackWriter/MessagePackWriter.Integers.cs
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/Serialization/MessagePack/MessagePackWriter/MessagePackWriter.Integers.cs
@@ -1,32 +1,169 @@
-using System;
-
 namespace AlgoSdk.MessagePack
 {
     public partial struct MessagePackWriter
     {
+        public void Write(sbyte value)
+        {
+            if (value >= 0)
+            {
+                Write((byte)value);
+            }
+            else
+            {
+                if (value >= MessagePackRange.MinFixNegativeInt)
+                {
+                    data.Add(unchecked((byte)value));
+                }
+                else
+                {
+                    data.Add(MessagePackCode.Int8);
+                    data.Add(unchecked((byte)value));
+                }
+            }
+        }
+
         public void Write(short value)
         {
-            throw new NotImplementedException();
+            if (value >= 0)
+            {
+                Write((ushort)value);
+            }
+            else
+            {
+                if (value >= MessagePackRange.MinFixNegativeInt)
+                {
+                    data.Add(unchecked((byte)value));
+                }
+                else if (value >= sbyte.MinValue)
+                {
+                    data.Add(MessagePackCode.Int8);
+                    data.Add(unchecked((byte)value));
+                }
+                else
+                {
+                    data.Add(MessagePackCode.Int16);
+                    WriteBigEndian(value);
+                }
+            }
         }
 
         public void Write(int value)
         {
-            throw new NotImplementedException();
+            if (value >= 0)
+            {
+                Write((uint)value);
+            }
+            else
+            {
+                if (value >= MessagePackRange.MinFixNegativeInt)
+                {
+                    data.Add(unchecked((byte)value));
+                }
+                else if (value >= sbyte.MinValue)
+                {
+                    data.Add(MessagePackCode.Int8);
+                    data.Add(unchecked((byte)value));
+                }
+                else if (value >= short.MinValue)
+                {
+                    data.Add(MessagePackCode.Int16);
+                    WriteBigEndian((short)value);
+                }
+                else
+                {
+                    data.Add(MessagePackCode.Int32);
+                    WriteBigEndian(value);
+                }
+            }
         }
 
         public void Write(long value)
         {
-            throw new NotImplementedException();
+            if (value >= 0)
+            {
+                Write((ulong)value);
+            }
+            else
+            {
+                if (value >= MessagePackRange.MinFixNegativeInt)
+                {
+                    data.Add(unchecked((byte)value));
+                }
+                else if (value >= sbyte.MinValue)
+                {
+                    data.Add(MessagePackCode.Int8);
+                    data.Add(unchecked((byte)value));
+                }
+                else if (value >= short.MinValue)
+                {
+                    data.Add(MessagePackCode.Int16);
+                    WriteBigEndian((short)value);
+                }
+                else if (value >= int.MinValue)
+                {
+                    data.Add(MessagePackCode.Int32);
+                    WriteBigEndian((int)value);
+                }
+                else
+                {
+                    data.Add(MessagePackCode.Int64);
+                    WriteBigEndian(value);
+                }
+            }
+        }
+
+        public void Write(byte value)
+        {
+            if (value <= MessagePackRange.MaxFixPositiveInt)
+            {
+                data.Add(unchecked((byte)value));
+            }
+            else
+            {
+                data.Add(MessagePackCode.UInt8);
+                data.Add(unchecked((byte)value));
+            }
         }
 
         public void Write(ushort value)
         {
-            throw new NotImplementedException();
+            if (value <= MessagePackRange.MaxFixPositiveInt)
+            {
+                data.Add(unchecked((byte)value));
+            }
+            else if (value <= byte.MaxValue)
+            {
+                data.Add(MessagePackCode.UInt8);
+                data.Add(unchecked((byte)value));
+            }
+            else
+            {
+                data.Add(MessagePackCode.UInt16);
+                WriteBigEndian((ushort)value);
+            }
         }
 
         public void Write(uint value)
         {
-            throw new NotImplementedException();
+            if (value <= MessagePackRange.MaxFixPositiveInt)
+            {
+                data.Add(unchecked((byte)value));
+            }
+            else if (value <= byte.MaxValue)
+            {
+                data.Add(MessagePackCode.UInt8);
+                data.Add(unchecked((byte)value));
+            }
+            else if (value <= ushort.MaxValue)
+            {
+                data.Add(MessagePackCode.UInt16);
+                WriteBigEndian((ushort)value);
+            }
+            else
+            {
+                data.Add(MessagePackCode.UInt32);
+                WriteBigEndian((uint)value);
+            }
         }
 
         public void Write(ulong value)
