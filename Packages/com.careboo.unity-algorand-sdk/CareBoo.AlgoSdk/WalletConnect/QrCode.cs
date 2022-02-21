c76e3961f63be3fa7e064fece96f4842361e8335
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2022-02-21 15:36:06 -0800

    feat(qrcode): add `QrCode` static class for generating `Texture2D`

---
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk/WalletConnect/QrCode.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using ZXing;
+using ZXing.QrCode;
+
+namespace AlgoSdk.WalletConnect
+{
+    public static class QrCode
+    {
+        public static Texture2D Generate(string text)
+        {
+            var encoded = new Texture2D(256, 256);
+            var color32 = Encode(text, encoded.width, encoded.height);
+            encoded.SetPixels32(color32);
+            encoded.Apply();
+            return encoded;
+        }
+
+        static Color32[] Encode(string textForEncoding, int width, int height)
+        {
+            var writer = new BarcodeWriter
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new QrCodeEncodingOptions
+                {
+                    Height = height,
+                    Width = width
+                }
+            };
+            return writer.Write(textForEncoding);
+        }
+    }
+}
