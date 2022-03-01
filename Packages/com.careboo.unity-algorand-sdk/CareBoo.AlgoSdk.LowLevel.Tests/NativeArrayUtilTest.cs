958506b8ba44ff7b5ac1b5f0e430cea0f5b1a192
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2022-02-28 18:56:39 -0800

    feat(lowlevel): add `NativeArrayUtil.ConcatAll`
    also remove unused struct `NativeReferenceOfDisposable` and create new assembly for lowlevel tests.
    
    BREAKING CHANGE: Remove unused struct `NativeReferenceOfDisposable` from AlgoSdk.LowLevel

---
+++ Packages/com.careboo.unity-algorand-sdk/CareBoo.AlgoSdk.LowLevel.Tests/NativeArrayUtilTest.cs
@@ -0,0 +1,31 @@
+using System.Linq;
+using AlgoSdk.LowLevel;
+using NUnit.Framework;
+using Unity.Collections;
+
+public class NativeArrayUtilTest
+{
+    [Test]
+    public void ConcatAllShouldReturnArrayWithConcattedBytes()
+    {
+        var input = RandomBytes();
+        var expected = input.SelectMany(b => b).ToArray();
+        using var actualNative = NativeArrayUtil.ConcatAll(input, Allocator.Temp);
+        var actual = actualNative.ToArray();
+        Assert.True(expected.SequenceEqual(actual));
+    }
+
+    byte[][] RandomBytes()
+    {
+        var random = new System.Random();
+        var numArrays = UnityEngine.Random.Range(3, 10);
+        var result = new byte[numArrays][];
+        for (var i = 0; i < numArrays; i++)
+        {
+            var element = new byte[UnityEngine.Random.Range(128, 1024)];
+            random.NextBytes(element);
+            result[i] = element;
+        }
+        return result;
+    }
+}
