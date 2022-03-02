15cd1a444b3f000f6b3aa319b4df079b87e880c9
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2022-03-02 00:18:17 -0800

    ci(logs): add logs to help determine pwd

--- scripts/setupUnityPackage.sh
+++ scripts/setupUnityPackage.sh
@@ -2,7 +2,9 @@
 
 set -euo pipefail
 
+echo "changing directory to Unity.AlgoSdk, pwd: $(pwd)"
 cd Unity.AlgoSdk
+echo "now in directory: $(pwd)"
 
 algosdk_src="Packages/com.careboo.unity-algorand-sdk"
 algosdk_target="Assets/AlgoSDK"
@@ -13,11 +15,13 @@ unitask_target="Assets/UniTask"
 
 for item in $algosdk_src/*
 do
+    echo "now in directory: $(pwd)"
     cp -RvL "$item" "$algosdk_target/"
 done
 
 for item in $unitask_src/*
 do
+    echo "now in directory: $(pwd)"
     cp -RvL "$item" "$unitask_target/"
 done
 
