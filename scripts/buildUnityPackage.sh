509283f66826b09240b317cdb8e8b79d7c34d1ad
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2022-03-01 23:00:12 -0800

    build(unitypackage): update buildUnityPackage to only setup project for building the unity package

--- scripts/buildUnityPackage.sh
+++ scripts/buildUnityPackage.sh
@@ -3,45 +3,38 @@
 set -euo pipefail
 
 
-echo "Building unity package using unity packager"
-
-
-algosdk_dir="unity-algorand-sdk"
-mkdir -p "$algosdk_dir"
+algosdk_target="Unity.AlgoSdk/Assets/unity-algorand-sdk"
 for entry in Runtime Editor Tests README.md CHANGELOG.md LICENSE.md "Third Party Notices.md" package.json
 do
-cp -fRv "$entry" "$algosdk_dir"
-cp -fv "$entry.meta" "$algosdk_dir"
+cp -fRv "$entry" "$algosdk_target"
+cp -fv "$entry.meta" "$algosdk_target"
 done
 for entry in "Documentation~"
 do
-cp -fRv "$entry" "$algosdk_dir"
+cp -fRv "$entry" "$algosdk_target"
 done
 
-unitask_root="Unity.AlgoSdk/Packages/com.cysharp.unitask"
-unitask_dir="UniTask"
-mkdir -p "$unitask_dir"
+unitask_src="Unity.AlgoSdk/Packages/com.cysharp.unitask"
+unitask_target="Unity.AlgoSdk/Assets/UniTask"
+mkdir -p "$unitask_target"
 for entry in Runtime Editor package.json
 do
-    cp -fRv "$unitask_root/$entry" "$unitask_dir"
-    cp -fv "$unitask_root/$entry.meta" "$unitask_dir"
+    cp -fRv "$unitask_src/$entry" "$unitask_target"
+    cp -fv "$unitask_src/$entry.meta" "$unitask_target"
 done
-
-
-tmpdir="$HOME"
-packager="UnityPackager"
-packager_dir="$tmpdir/$packager"
-mkdir -p "$packager_dir"
-wget -c "https://github.com/TwoTenPvP/UnityPackager/archive/refs/tags/v1.2.5.tar.gz" -O - | tar -xz -C "$packager_dir"
-packager_dir="$packager_dir/$packager-1.2.5"
 
 
 mkdir -p dist
-workdir="$(pwd)"
-cd "$packager_dir"
-ls "$workdir"
-ls "$workdir/$algosdk_dir"
-ls "$workdir/$unitask_dir"
-echo "Packing $workdir/$algosdk_dir and $workdir/$unitask_dir"
-dotnet run -f netcoreapp2.1 --project "$packager" pack "$workdir/dist/unity-algorand-sdk.unitypackage" "$workdir/$algosdk_dir" "$workdir/$unitask_dir"
-cd -
+touch dist/packageMetaFiles
+
+echo "$algosdk_target.meta" >> dist/packageMetaFiles
+for metaFile in $algosdk_target/**/*.meta
+do
+    echo "$metaFile" >> dist/packageMetaFiles
+done
+
+echo "$unitask_target.meta" >> dist/packageMetaFiles
+for metaFile in $unitask_target/**/*.meta
+do
+    echo "$metaFile" >> dist/packageMetaFiles
+done
