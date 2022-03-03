b51eb28617d772b9e25f2db8980d81d90263e767
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2022-03-02 22:36:17 -0800

    build(unitypackage): be verbose with rm

--- scripts/setupUnityPackage.sh
+++ scripts/setupUnityPackage.sh
@@ -8,13 +8,13 @@ algosdk_src="Packages/com.careboo.unity-algorand-sdk"
 algosdk_target="Assets/AlgoSdk"
 
 cp -RvL $algosdk_src/* $algosdk_target
-ls -1 $algosdk_src | grep -v "package.json" | xargs -I {} rm -rf $algosdk_src/{}
+ls -1 $algosdk_src | grep -v "package.json" | xargs -I {} rm -rfv $algosdk_src/{}
 
 
 unitask_src="Packages/com.cysharp.unitask"
 unitask_target="Assets/UniTask"
 
 cp -RvL $unitask_src/* $unitask_target
-ls -1 $unitask_src | grep -v "package.json" | xargs -I {} rm -rf $unitask_src/{}
+ls -1 $unitask_src | grep -v "package.json" | xargs -I {} rm -rfv $unitask_src/{}
 
 cd -
