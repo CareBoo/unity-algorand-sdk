0b17ea1758dd53960a82841c4ff3caa130ab2f09
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2022-03-04 13:25:56 -0800

    refactor(assemblies): move libraries to their own assemblies to simplify things
    BREAKING CHANGE: `zxing.net` and `websocket-sharp` are now in their own assemblies. You will need to
    update them if you were using the `ZXing` or `WebSocket` namespaces.

--- Runtime/websocket-sharp/websocket-sharp/WebSocketException.cs
+++ Runtime/websocket-sharp/websocket-sharp/WebSocketException.cs
@@ -42,44 +42,44 @@ namespace WebSocketSharp
 
     #endregion
 
-    #region Internal Constructors
+    #region Public Constructors
 
-    internal WebSocketException ()
+    public WebSocketException ()
       : this (CloseStatusCode.Abnormal, null, null)
     {
     }
 
-    internal WebSocketException (Exception innerException)
+    public WebSocketException (Exception innerException)
       : this (CloseStatusCode.Abnormal, null, innerException)
     {
     }
 
-    internal WebSocketException (string message)
+    public WebSocketException (string message)
       : this (CloseStatusCode.Abnormal, message, null)
     {
     }
 
-    internal WebSocketException (CloseStatusCode code)
+    public WebSocketException (CloseStatusCode code)
       : this (code, null, null)
     {
     }
 
-    internal WebSocketException (string message, Exception innerException)
+    public WebSocketException (string message, Exception innerException)
       : this (CloseStatusCode.Abnormal, message, innerException)
     {
     }
 
-    internal WebSocketException (CloseStatusCode code, Exception innerException)
+    public WebSocketException (CloseStatusCode code, Exception innerException)
       : this (code, null, innerException)
     {
     }
 
-    internal WebSocketException (CloseStatusCode code, string message)
+    public WebSocketException (CloseStatusCode code, string message)
       : this (code, message, null)
     {
     }
 
-    internal WebSocketException (
+    public WebSocketException (
       CloseStatusCode code, string message, Exception innerException
     )
       : base (message ?? code.GetMessage (), innerException)
