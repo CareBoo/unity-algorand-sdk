3d76a4b551c0ca671216bb520c79a09482eca410
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2022-03-04 18:42:00 -0800

    refactor: move json, messagepack, and encoding logic into their own assemblies
    Created 3 assemblies: `CareBoo.AlgoSdk.Json`, `CareBoo.AlgoSdk.MessagePack`,
    `CareBoo.AlgoSdk.Encoding` to store that lower level logic
    
    BREAKING CHANGE: Json, MessagePack, and Encoding logic has been moved. If you were referencing them
    before you will have to target them in new assemblies now.

--- Runtime/Unity.Collections.Extensions/AssemblyInfo.cs
+++ Runtime/Unity.Collections.Extensions/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("CareBoo.AlgoSdk")]
 [assembly: InternalsVisibleTo("CareBoo.AlgoSdk.Collections")]
+[assembly: InternalsVisibleTo("CareBoo.AlgoSdk.Json")]
