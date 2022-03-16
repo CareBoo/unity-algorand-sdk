6470432546796e368c5df0e6c923648564d02448
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2022-03-15 19:59:43 -0500

    build(unitypackage): add move packages logic

--- Unity.AlgoSdk/Assets/Editor/UnityPackage.cs
+++ Unity.AlgoSdk/Assets/Editor/UnityPackage.cs
@@ -4,19 +4,35 @@ public static class UnityPackage
 {
     const string packageName = "unity-algorand-sdk";
 
-    static readonly string[] exportedAssets = new[]
-    {
-        "Packages/com.careboo.unity-algorand-sdk",
-        "Packages/com.cysharp.unitask"
-    };
-
     [MenuItem("AlgoSdk/Package")]
     public static void Build()
     {
+        MovePackagesIntoAssets();
         AssetDatabase.ExportPackage(
-            assetPathNames: exportedAssets,
+            assetPathName: "Assets/AlgoSdk",
             fileName: packageName + ".unitypackage",
             ExportPackageOptions.Recurse | ExportPackageOptions.IncludeDependencies
         );
+        MovePackagesBackIntoPackages();
+    }
+
+    static void MovePackagesIntoAssets()
+    {
+        AssetDatabase.MoveAsset("Packages/com.careboo.unity-algorand-sdk", "Assets/AlgoSdk");
+        AssetDatabase.CreateFolder("Assets/AlgoSdk/Third Party", "Third Party");
+
+        AssetDatabase.MoveAsset("Packages/com.cysharp.unitask", "Assets/AlgoSdk/Third Party/UniTask");
+        AssetDatabase.MoveAsset("Assets/AlgoSdk/Runtime/websocket-sharp", "Assets/AlgoSdk/Third Party/websocket-sharp");
+        AssetDatabase.MoveAsset("Assets/AlgoSdk/Runtime/zxing.unity", "Assets/AlgoSdk/Third Party/zxing.unity");
+    }
+
+    static void MovePackagesBackIntoPackages()
+    {
+        AssetDatabase.MoveAsset("Assets/AlgoSdk/Third Party/UniTask", "Packages/com.cysharp.unitask");
+        AssetDatabase.MoveAsset("Assets/AlgoSdk/Third Party/websocket-sharp", "Assets/AlgoSdk/Runtime/websocket-sharp");
+        AssetDatabase.MoveAsset("Assets/AlgoSdk/Third Party/zxing.unity", "Assets/AlgoSdk/Runtime/zxing.unity");
+
+        AssetDatabase.DeleteAsset("Assets/AlgoSdk/Third Party");
+        AssetDatabase.MoveAsset("Assets/AlgoSdk", "Packages/com.careboo.unity-algorand-sdk");
     }
 }
