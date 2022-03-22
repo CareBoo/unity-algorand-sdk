19f139db99e03e389d7f8abfad45e1bdb3b0667d
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2022-03-21 17:01:54 -0700

    feat(account): add `IAccount`, `ISigner`, and `IAsyncSigner` interfaces
    These are used to start implementing accounts for all types of signing.
    
    fix #82

--- Runtime/CareBoo.AlgoSdk/Transaction/ITransaction.cs
+++ Runtime/CareBoo.AlgoSdk/Transaction/ITransaction.cs
@@ -11,27 +11,27 @@ namespace AlgoSdk
         /// <summary>
         /// Paid by the sender to the FeeSink to prevent denial-of-service. The minimum fee on Algorand is currently 1000 microAlgos.
         /// </summary>
-        ulong Fee { get; }
+        ulong Fee { get; set; }
 
         /// <summary>
         /// The first round for when the transaction is valid. If the transaction is sent prior to this round it will be rejected by the network.
         /// </summary>
-        ulong FirstValidRound { get; }
+        ulong FirstValidRound { get; set; }
 
         /// <summary>
         /// The hash of the genesis block of the network for which the transaction is valid.
         /// </summary>
-        GenesisHash GenesisHash { get; }
+        GenesisHash GenesisHash { get; set; }
 
         /// <summary>
         /// The ending round for which the transaction is valid. After this round, the transaction will be rejected by the network.
         /// </summary>
-        ulong LastValidRound { get; }
+        ulong LastValidRound { get; set; }
 
         /// <summary>
         /// The address of the account that pays the fee and amount.
         /// </summary>
-        Address Sender { get; }
+        Address Sender { get; set; }
 
         /// <summary>
         /// Specifies the type of transaction. This value is automatically generated using any of the developer tools.
@@ -41,27 +41,27 @@ namespace AlgoSdk
         /// <summary>
         /// The human-readable string that identifies the network for the transaction. The genesis ID is found in the genesis block.
         /// </summary>
-        FixedString32Bytes GenesisId { get; }
+        FixedString32Bytes GenesisId { get; set; }
 
         /// <summary>
         /// The group specifies that the transaction is part of a group and, if so, specifies the hash of the transaction group. See <see cref="Transaction.GetGroupId"/>.
         /// </summary>
-        Sha512_256_Hash Group { get; }
+        Sha512_256_Hash Group { get; set; }
 
         /// <summary>
         /// A lease enforces mutual exclusion of transactions. If this field is nonzero, then once the transaction is confirmed, it acquires the lease identified by the (Sender, Lease) pair of the transaction until the LastValid round passes. While this transaction possesses the lease, no other transaction specifying this lease can be confirmed. A lease is often used in the context of Algorand Smart Contracts to prevent replay attacks.
         /// </summary>
-        Sha512_256_Hash Lease { get; }
+        Sha512_256_Hash Lease { get; set; }
 
         /// <summary>
         /// Any data up to 1000 bytes.
         /// </summary>
-        byte[] Note { get; }
+        byte[] Note { get; set; }
 
         /// <summary>
         /// Specifies the authorized address. This address will be used to authorize all future transactions.
         /// </summary>
-        Address RekeyTo { get; }
+        Address RekeyTo { get; set; }
 
         /// <summary>
         /// Copy this transactions fields to a <see cref="Transaction"/> which contains all possible transaction fields.
