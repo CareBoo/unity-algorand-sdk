e4cc4688299763ef36df0969ebd4dafa6aed871a
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2022-03-23 20:21:01 -0700

    feat(transaction): add Indexer TransactionApplication model

--- Runtime/CareBoo.AlgoSdk/Api/Models/StateSchema.cs
+++ Runtime/CareBoo.AlgoSdk/Api/Models/StateSchema.cs
@@ -6,24 +6,35 @@ namespace AlgoSdk
     /// <summary>
     /// The <see cref="StateSchema"/> object is only required for the create application call transaction. The <see cref="StateSchema"/> object must be fully populated for both the <see cref="IAppCallTxn.GlobalStateSchema"/> and <see cref="IAppCallTxn.LocalStateSchema"/> objects.
     /// </summary>
-    [AlgoApiObject]
-    [Serializable]
+    [AlgoApiObject, Serializable]
     public partial struct StateSchema
         : IEquatable<StateSchema>
     {
+        [SerializeField, Tooltip("Maximum number of integer values that may be stored in the [global || local] application key/value store. Immutable.")]
+        ulong numByteSlices;
+
+        [SerializeField, Tooltip("Maximum number of byte slices values that may be stored in the [global || local] application key/value store. Immutable.")]
+        ulong numUints;
+
         /// <summary>
         /// Maximum number of integer values that may be stored in the [global || local] application key/value store. Immutable.
         /// </summary>
         [AlgoApiField("num-byte-slice", "nbs")]
-        [Tooltip("Maximum number of integer values that may be stored in the [global || local] application key/value store. Immutable.")]
-        public ulong NumByteSlices;
+        public ulong NumByteSlices
+        {
+            get => numByteSlices;
+            set => numByteSlices = value;
+        }
 
         /// <summary>
         /// Maximum number of byte slices values that may be stored in the [global || local] application key/value store. Immutable.
         /// </summary>
         [AlgoApiField("num-uint", "nui")]
-        [Tooltip("Maximum number of byte slices values that may be stored in the [global || local] application key/value store. Immutable.")]
-        public ulong NumUints;
+        public ulong NumUints
+        {
+            get => numUints;
+            set => numUints = value;
+        }
 
         public bool Equals(StateSchema other)
         {
