9e04d57011263d4aaeda14b42708607509d98945
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2022-06-12 15:42:23 -0700

    docs(wallets): add new sample showing how to connect wallets to the Unity Editor

---
+++ Samples~/EditorWallets/Runtime/AsyncSignerAccountObject.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+
+namespace AlgoSdk
+{
+    public abstract class AsyncSignerAccountObject
+        : BaseAccountObject
+        , IAsyncSigner
+    {
+        public abstract UniTask<SignedTxn<T>[]> SignTxnsAsync<T>(T[] txns, TxnIndices txnsToSign, CancellationToken cancellationToken = default) where T : ITransaction, IEquatable<T>;
+    }
+}
---
+++ Samples~/EditorWallets/Runtime/WalletConnectAccountObject.cs
@@ -0,0 +1,68 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace AlgoSdk.WalletConnect
+{
+    public class WalletConnectAccountObject
+        : AsyncSignerAccountObject
+        , IWalletConnectAccount
+    {
+        [SerializeField]
+        WalletConnectAccount account = new WalletConnectAccount();
+
+        /// <inheritdoc />
+        public override Address Address => account.Address;
+
+        /// <inheritdoc />
+        public SessionData SessionData
+        {
+            get => account.SessionData;
+            set => account.SessionData = value;
+        }
+
+        /// <inheritdoc />
+        public string BridgeUrl
+        {
+            get => account.BridgeUrl;
+            set => account.BridgeUrl = value;
+        }
+
+        /// <inheritdoc />
+        public ClientMeta DappMeta
+        {
+            get => account.DappMeta;
+            set => account.DappMeta = value;
+        }
+
+        /// <inheritdoc />
+        public SessionStatus ConnectionStatus => account.ConnectionStatus;
+
+        /// <inheritdoc />
+        public UniTask BeginSession() => account.BeginSession();
+
+        /// <inheritdoc />
+        public void EndSession() => account.EndSession();
+
+        /// <inheritdoc />
+        public void ResetSessionData() => account.ResetSessionData();
+
+        /// <inheritdoc />
+        public HandshakeUrl RequestWalletConnection() => account.RequestWalletConnection();
+
+        /// <inheritdoc />
+        public UniTask WaitForWalletApproval() => account.WaitForWalletApproval();
+
+        /// <inheritdoc />
+        public void DisconnectWallet(string reason = default) => account.DisconnectWallet(reason);
+
+        /// <inheritdoc />
+        public override UniTask<SignedTxn<T>[]> SignTxnsAsync<T>(
+            T[] txns, TxnIndices txnsToSign,
+            CancellationToken cancellationToken = default
+            )
+        {
+            return account.SignTxnsAsync(txns, txnsToSign, cancellationToken);
+        }
+    }
+}
