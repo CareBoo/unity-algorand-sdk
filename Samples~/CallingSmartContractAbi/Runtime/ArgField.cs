6783776c168cdb06430ea4063ac77b80c191d325
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2022-07-13 22:43:52 -0700

    docs(abi): add `CallingSmartContractAbi` sample

---
+++ Samples~/CallingSmartContractAbi/Runtime/ArgField.cs
@@ -0,0 +1,20 @@
+using System;
+using AlgoSdk.Experimental.Abi;
+using UnityEngine.UIElements;
+
+
+public abstract partial class ArgField : VisualElement
+{
+    public abstract IAbiValue Value { get; }
+
+    public static ArgField Create(string label, IAbiType type) => type switch
+    {
+        ReferenceType refType when refType.Type == AbiReferenceType.Account => new Account(label),
+        VariableArrayType variableArrayType => new Array(label, variableArrayType),
+        TupleType tupleType => new Tuple(label, tupleType),
+        StringType stringType => new String(label),
+        UIntType uintType when uintType.N == 64 => new UInt64(label),
+        TransactionReferenceType txnRefType => new Txn(label, txnRefType),
+        _ => throw new NotSupportedException()
+    };
+}
---
+++ Samples~/CallingSmartContractAbi/Runtime/MethodField.cs
@@ -0,0 +1,33 @@
+using AlgoSdk.Experimental.Abi;
+using UnityEngine.UIElements;
+
+public class MethodField : VisualElement
+{
+    private readonly ArgField[] argumentFields;
+
+    public MethodField(Method method)
+    {
+        argumentFields = new ArgField[method.Arguments.Length];
+
+        var argFoldout = new Foldout { text = "Arguments" };
+        Add(argFoldout);
+
+        for (var i = 0; i < method.Arguments.Length; i++)
+        {
+            var arg = method.Arguments[i];
+            var argField = ArgField.Create(arg.Name, arg.Type);
+            argumentFields[i] = argField;
+            argFoldout.contentContainer.Add(argField);
+        }
+    }
+
+    public IAbiValue[] GetAbiValues()
+    {
+        var abiValues = new IAbiValue[argumentFields.Length];
+        for (var i = 0; i < abiValues.Length; i++)
+        {
+            abiValues[i] = argumentFields[i].Value;
+        }
+        return abiValues;
+    }
+}
