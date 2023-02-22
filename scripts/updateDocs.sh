ebe81dfea22e9f06db6132c50c270daa3ec7ce8d
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2023-02-22 07:20:35 -0800

    chore(scripts): fix updateDocs.sh to delete old files

--- scripts/updateDocs.sh
+++ scripts/updateDocs.sh
@@ -2,5 +2,6 @@
 
 set -euo pipefail
 
+find Algorand.Unity.AssetStore/Assets/Algorand.Unity/Documentation -type f \! -name '*.meta' -delete
 docfx .docfx/docfx.json -t statictoc
 cp -r _site/ Algorand.Unity.AssetStore/Assets/Algorand.Unity/Documentation/
