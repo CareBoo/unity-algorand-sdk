e707d23e9ed6af7cf91cb8ef0db1aba11336b9d9
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2023-09-24 23:55:14 -0700

    feat(libsodium): :sparkles: add pwhash, secretbox apis

--- Runtime/Algorand.Unity.Crypto/Interop/sodium/sodium+Ed25519.cs
+++ Runtime/Algorand.Unity.Crypto/Interop/sodium/sodium+Ed25519.cs
@@ -5,11 +5,26 @@ namespace Algorand.Unity.Crypto
 {
     internal static unsafe partial class sodium
     {
+        internal const int crypto_sign_ed25519_BYTES = 64;
+        internal const int crypto_sign_ed25519_PUBLICKEYBYTES = 32;
+        internal const int crypto_sign_ed25519_SECRETKEYBYTES = 64;
+        internal const int crypto_sign_ed25519_SEEDBYTES = 32;
+
+        [DllImport(Library, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int crypto_sign_ed25519_sk_to_pk(
+            void* pk,
+            void* sk);
+
         [DllImport(Library, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int crypto_sign_ed25519_seed_keypair(
             void* pk,
-            IntPtr sk,
+            void* sk,
             void* seed);
+
+        [DllImport(Library, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int crypto_sign_ed25519_sk_to_seed(
+            void* seed,
+            void* sk);
 
         [DllImport(Library, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int crypto_sign_ed25519_detached(
@@ -17,7 +32,7 @@ namespace Algorand.Unity.Crypto
             out ulong signatureLength_p,
             void* message,
             ulong messageLength,
-            IntPtr sk);
+            void* sk);
 
         [DllImport(Library, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int crypto_sign_ed25519_verify_detached(
