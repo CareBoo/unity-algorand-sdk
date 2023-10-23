889afa9334f92ca2dcecb9746d2655006dad6f34
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2023-10-22 21:10:50 -0700

    docs: :construction_worker: add `DocUrl` util class to reference for `HelpURL`s

---
+++ Runtime/Algorand.Unity/Util/DocUrl.cs
@@ -0,0 +1,8 @@
+namespace Algorand.Unity
+{
+    internal static class DocUrl
+    {
+        internal const string Base = "https://careboo.github.io/unity-algorand-sdk/5.0/";
+        internal const string Api = Base + "api/";
+    }
+}
