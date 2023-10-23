aba4efb53c72771739072800864b3779e3bbdbdd
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2023-10-22 21:27:43 -0700

    feat(kmd): :sparkles: add `KmdClientAsset` to store kmd info

--- Runtime/Algorand.Unity/Clients/Kmd/KmdClientAsset.cs
+++ Runtime/Algorand.Unity/Clients/Kmd/KmdClientAsset.cs
@@ -1,10 +1,26 @@
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 namespace Algorand.Unity
 {
+    [HelpURL(DocUrl.Api + "Algorand.Unity.KmdClientAsset.html")]
     [CreateAssetMenu(menuName = "Algorand/Kmd Client")]
     public class KmdClientAsset : ScriptableObject
     {
+        [ContextMenuItem("Test Connection", nameof(TestConnection))]
         public KmdClient client;
+
+        private void TestConnection()
+        {
+            TestConnectionAsync().Forget();
+        }
+
+        private async UniTaskVoid TestConnectionAsync()
+        {
+            var (error, result) = await client.Versions();
+
+            if (error) Debug.LogError(error);
+            else Debug.Log($"Kmd connection healthy. Versions: [{string.Join(", ", result.Versions)}]");
+        }
     }
 }
