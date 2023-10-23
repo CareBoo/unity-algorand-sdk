6a4d301c31d36bf3372eb91b835490e6fbae0664
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2023-10-22 21:27:14 -0700

    feat(algod): :sparkles: add `AlgodClientAsset` to be used for storing algod info

--- Runtime/Algorand.Unity/Clients/Algod/AlgodClientAsset.cs
+++ Runtime/Algorand.Unity/Clients/Algod/AlgodClientAsset.cs
@@ -1,10 +1,27 @@
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 namespace Algorand.Unity
 {
+    [HelpURL(DocUrl.Api + "Algorand.Unity.AlgodClientAsset.html")]
     [CreateAssetMenu(menuName = "Algorand/Algod Client")]
     public class AlgodClientAsset : ScriptableObject
     {
+        public AlgorandNetwork network;
+
+        [ContextMenuItem("Test Connection", nameof(TestConnection))]
         public AlgodClient client;
+
+        private void TestConnection()
+        {
+            TestConnectionAsync().Forget();
+        }
+
+        private async UniTaskVoid TestConnectionAsync()
+        {
+            var result = await client.HealthCheck();
+            if (result.Error) Debug.LogError(result.Error);
+            else Debug.Log("Algod connection healthy.");
+        }
     }
 }
