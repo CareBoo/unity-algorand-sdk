2bdc0da8ef68a62e4172a02bd4858731215f9b3d
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2023-10-24 01:03:30 -0700

    feat(abi): :sparkles: add `InterfaceImporter` for `interface.json` files describing abi interfaces

--- Editor/Algorand.Unity.Editor/Importers/Abi/InterfaceImporter.cs
+++ Editor/Algorand.Unity.Editor/Importers/Abi/InterfaceImporter.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 namespace Algorand.Unity.Experimental.Abi.Editor
 {
-    [ScriptedImporter(1, "contract.json")]
-    public class AbiContractImporter : ScriptedImporter
+    [ScriptedImporter(1, "interface.json")]
+    public class InterfaceImporter : ScriptedImporter
     {
         public override void OnImportAsset(AssetImportContext ctx)
         {
             var fileName = Path.GetFileNameWithoutExtension(ctx.assetPath);
             var json = File.ReadAllText(ctx.assetPath);
-            var asset = ScriptableObject.CreateInstance<ContractAsset>();
-            asset.contract = AlgoApiSerializer.DeserializeJson<Contract>(json);
+            var asset = ScriptableObject.CreateInstance<InterfaceAsset>();
+            asset.@interface = AlgoApiSerializer.DeserializeJson<Interface>(json);
             asset.name = fileName;
-            ctx.AddObjectToAsset("contract", asset);
+            ctx.AddObjectToAsset("interface", asset);
             ctx.SetMainObject(asset);
         }
     }
